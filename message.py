5085f36daff2028667f0a919c2071ac599d35c00
DhruvAnand <ananddhruv29@gmail.com>
2021-08-11 20:18:55 +0530

    Added info popup.
    > Added a popup for displaying information.

--- message.py
+++ message.py
@@ -3,7 +3,12 @@ from tkinter import messagebox
 
 def alert(alert_text):
     """ Displays an Alert box.  """
-    messagebox.showerror(title="Error opening File", message=alert_text)
+    messagebox.showerror(title="Oops", message=alert_text)
+
+
+def info(info_text):
+    """ Displays an Information box """
+    messagebox.showinfo(title="Information", message=info_text)
 
 
 if __name__ == '__main__':
