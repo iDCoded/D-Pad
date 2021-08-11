e5144dacbd39b8de3372aa2ec7e6a5c600e83fc5
DhruvAnand <ananddhruv29@gmail.com>
2021-08-11 20:13:31 +0530

    Updated Alert popup
    > Using Tkinter.Messagebox to display alerts.

--- message.py
+++ message.py
@@ -1,30 +1,9 @@
-from tkinter import *
+from tkinter import messagebox
 
 
 def alert(alert_text):
     """ Displays an Alert box.  """
-    def exit():
-        ROOT.destroy()
-
-    ROOT = Tk()
-
-    ROOT.title('Alert')
-
-    # Alert popup geometry
-    ROOT.geometry('300x150')
-    ROOT.minsize(300, 150)
-    ROOT.maxsize(300, 150)
-
-    alert_frame = LabelFrame(ROOT, text='Alert')
-    alert_frame.pack(fill='both', expand='yes')
-
-    alert_display = Label(alert_frame, text=alert_text)
-    alert_display.pack()
-
-    okay_button = Button(ROOT, text='Okay', command=exit)
-    okay_button.pack(side='right', anchor='se')
-
-    ROOT.mainloop()
+    messagebox.showerror(title="Error opening File", message=alert_text)
 
 
 if __name__ == '__main__':
